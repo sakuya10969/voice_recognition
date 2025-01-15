ee93d7697751416f9be19bab0e14caac89044c9e
sakuya10969 <sakuya.f20010618@gmail.com>
2025-01-15 14:10:55 +0900

    feat: エラー情報を強化

--- client/src/api/Api.jsx
+++ client/src/api/Api.jsx
@@ -7,23 +7,35 @@ const apiUrl = "https://20.78.45.241/transcribe";
 export const handleSendAudio = async (file) => {
     try {
         if (!file) {
-            alert('ファイルを選択してください');
+            alert("ファイルを選択してください");
             return;
         }
 
         const formData = new FormData();
-        formData.append('file', file);
+        formData.append("file", file);
 
         const response = await axios.post(apiUrl, formData, {
             headers: {
-                'Content-Type': 'multipart/form-data',
+                "Content-Type": "multipart/form-data",
             },
             timeout: 3600000
         });
 
         return response.data
     } catch (error) {
-        alert('ファイルのアップロードに失敗しました');
+        console.error("Error", {
+            message: error.message,
+            name: error.name,
+            code: error.code,
+            config: error.config,
+            request: error.request,
+            response: error.response ? {
+                data: error.response.data,
+                status: error.response.status,
+                headers: error.response.headers
+            } : null
+        });
+        alert("ファイルのアップロードに失敗しました");
         throw error;
     }
 }
